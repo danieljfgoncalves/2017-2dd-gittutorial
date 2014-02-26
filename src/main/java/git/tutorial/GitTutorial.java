10dc114a90f77d97c56cce9cfd161eb3cdd43112
Nuno Bettencourt <nmb@isep.ipp.pt>
2014-02-26 20:41:37 +0000

    Created Netbeans Project Configuration

---
+++ src/main/java/git/tutorial/GitTutorial.java
@@ -0,0 +1,17 @@
+package git.tutorial;
+
+public class GitTutorial {
+
+	public GitTutorial() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git Tutorial");
+
+	}
+
+}
