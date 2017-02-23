aba7fe29f36143302babf1b9c56032d24eef6295
Paulo Sousa <pag@isep.ipp.pt>
2017-02-23 15:31:09 +0000

    Cricket.java edited online with Bitbucket

--- src/main/java/git/tutorial/Cricket.java
+++ src/main/java/git/tutorial/Cricket.java
@@ -20,6 +20,10 @@ public class Cricket extends Animal {
         System.out.println("cri cri");
     }
 
+    public int numberOfAntenas() {
+        return 2;
+    }
+
     @Override
     public int numberOfLegs() {
         return NUMBER_OF_LEGS;
