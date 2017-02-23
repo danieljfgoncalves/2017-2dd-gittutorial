81203bef294bf7b10e7c3d075080effbf55f0a27
ivoferroo <1151159@isep.ipp.pt>
2017-02-23 15:44:36 +0000

    fixes dog class [refactor]

--- src/main/java/git/tutorial/Dog.java
+++ src/main/java/git/tutorial/Dog.java
@@ -1,16 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Package for tutorial purposes.
  */
 package git.tutorial;
 
 /**
  *
  * @author Meireles
+ * @author Ivo Ferro, 1151159
  */
 public class Dog extends Animal {
 
+    // Watch out for the warnings and code smells!
     private int numberOfLegs;
 
     public Dog() {
