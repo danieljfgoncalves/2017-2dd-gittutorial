3b0f946cc21b0b48cd097bbe07a5561e10391197
Miguel Silva <1150901@isep.ipp.pt>
2017-02-24 16:52:57 +0000

    Added "name" method to the Pig class.

--- src/main/java/git/tutorial/Pig.java
+++ src/main/java/git/tutorial/Pig.java
@@ -11,11 +11,11 @@ package git.tutorial;
  */
 public class Pig extends Animal {
 
-    public String colour(){
+    public String colour() {
         return "pink";
     }
 
-    public Pig () {
+    public Pig() {
     }
 
     public void makeSound(int loudness) {
@@ -25,8 +25,8 @@ public class Pig extends Animal {
     public int numberOfLegs() {
         return 5;
     }
-    
-    public void size(){
+
+    public void size() {
     }
 
     @Override
@@ -34,7 +34,11 @@ public class Pig extends Animal {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void mindOfAPig(){
+    public void mindOfAPig() {
         System.out.println("My dream is to be the tastiest BigMac!");
+    }
+
+    public String name() {
+        return "piggy";
     }
 }
